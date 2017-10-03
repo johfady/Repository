70c6e9f26db547b1251910239190f156796c062a
Youssef Wahba <Youssef@joe.local>
2017-10-04 00:54:26 +0200

    conflict in BorderButton.swift resolved

--- app-swoosh/app-swoosh/Views/BorderButton.swift
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -1,25 +1,19 @@
- //
+//
 //  BorderButton.swift
 //  app-swoosh
 //
-//  Created by Youssef Wahba on 9/24/17.
-//  Copyright © 2017 Youssef Wahba. All rights reserved.
+//  Created by Mark Price on 6/12/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
 //
 
 import UIKit
 
 class BorderButton: UIButton {
 
-<<<<<<< HEAD
     override func awakeFromNib() {
         super.awakeFromNib()
-        layer.borderWidth = 3.0
+        layer.borderWidth = 2.0
         layer.borderColor = UIColor.white.cgColor
     }
-=======
-  override func awakeFromNib() {
-        super.awakeFromNib()
-        layer.borderWidth = 3.0
-        layer.borderColor = UIColor.white.cgColor
->>>>>>> 834a46486a775191509ebad2be4d260cd19f0404
+
 }
