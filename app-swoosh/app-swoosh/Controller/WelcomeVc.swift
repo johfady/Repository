35cbf0fc61317cf2a08be2cad39c71c7f353bdb9
Youssef Wahba <Youssef@joe.local>
2017-10-05 16:08:39 +0200

    adding new uiview controllers

---
+++ app-swoosh/app-swoosh/Controller/WelcomeVc.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Youssef Wahba on 9/24/17.
+//  Copyright © 2017 Youssef Wahba. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVc: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    
+    @IBOutlet weak var BgImg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+    @IBAction func UnwindfromskillVC(Unwindsegue:UIStoryboardSegue){}
+
+
+}
+
---
+++ app-swoosh/app-swoosh/Controller/leguevc.swift
@@ -0,0 +1,35 @@
+//
+//  leguevc.swift
+//  app-swoosh
+//
+//  Created by Youssef Wahba on 10/4/17.
+//  Copyright © 2017 Youssef Wahba. All rights reserved.
+//
+
+import UIKit
+
+class leguevc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
